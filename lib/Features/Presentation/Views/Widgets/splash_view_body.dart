814f9ee84f8dada67d4615e44e7ec9cfe3768e51
Mohammad Adel Metwalli <batootadel6@gmail.com>
2023-05-22 23:19:08 +0300

    first commit

---
+++ lib/Features/Presentation/Views/Widgets/splash_view_body.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class SplashViewBody extends StatelessWidget
+{
+  const SplashViewBody({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Column(
+      children: const [
+
+      ],
+    );
+  }
+}
