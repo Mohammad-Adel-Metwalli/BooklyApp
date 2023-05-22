84627cbc2df227254295f567906bee72701a58c5
Mohammad Adel Metwalli <batootadel6@gmail.com>
2023-05-23 02:09:15 +0300

    Refactor init state & navigate to home page

--- lib/Features/Presentation/Views/Widgets/splash_view_body.dart
+++ lib/Features/Presentation/Views/Widgets/splash_view_body.dart
@@ -1,6 +1,9 @@
 import 'package:bookly_app/Core/Utils/assets.dart';
 import 'package:bookly_app/Features/Presentation/Views/Widgets/sliding_text.dart';
+import 'package:bookly_app/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../../../Home/Presentation/Views/home_view.dart';
 
 class SplashViewBody extends StatefulWidget
 {
@@ -20,6 +23,20 @@ class _SplashViewBodyState extends State<SplashViewBody> with SingleTickerProvid
   {
     super.initState();
 
+    initSlidingAnimation();
+    navigateToHome();
+  }
+
+  void navigateToHome()
+  {
+    Future.delayed(const Duration(seconds: 4), ()
+    {
+      Get.to(() => const HomeView(), transition: Transition.zoom, duration: durationOfPageTransition);
+    });
+  }
+
+  void initSlidingAnimation()
+  {
     animationController = AnimationController(vsync: this, duration: const Duration(seconds: 2));
     slidingAnimation = Tween<Offset>(begin: const Offset(0,20), end: Offset.zero).animate(animationController);
 
