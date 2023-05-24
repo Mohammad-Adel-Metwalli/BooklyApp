125b359a48db93303e2ea778cdbfc890be30772b
Mohammad Adel Metwalli <batootadel6@gmail.com>
2023-05-25 02:31:40 +0300

    Create book_details_view.dart

---
+++ lib/Features/Home/Presentation/Views/book_details_view.dart
@@ -0,0 +1,15 @@
+import 'package:bookly_app/Features/Home/Presentation/Views/Widgets/book_details_view_body.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsView extends StatelessWidget
+{
+  const BookDetailsView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return const Scaffold(
+        body: BookDetailsViewBody()
+    );
+  }
+}
