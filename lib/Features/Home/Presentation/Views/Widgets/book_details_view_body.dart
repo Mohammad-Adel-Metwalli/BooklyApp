c77be015f0f86f96eb31b58731b63f3c123fdfe0
Mohammad Adel Metwalli <batootadel6@gmail.com>
2023-05-26 01:11:53 +0300

    Refactor book details view

--- lib/Features/Home/Presentation/Views/Widgets/book_details_view_body.dart
+++ lib/Features/Home/Presentation/Views/Widgets/book_details_view_body.dart
@@ -1,10 +1,6 @@
-import 'package:bookly_app/Features/Home/Presentation/Views/Widgets/books_details_list_view.dart';
-import 'package:bookly_app/Features/Home/Presentation/Views/Widgets/custom_list_view_item.dart';
-import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/Features/Home/Presentation/Views/Widgets/similar_books_section.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'book_rating_only.dart';
-import 'books_action.dart';
+import 'books_details_section.dart';
 import 'custom_book_details_app_bar.dart';
 
 class BookDetailsViewBody extends StatelessWidget
@@ -14,55 +10,26 @@ class BookDetailsViewBody extends StatelessWidget
   @override
   Widget build(BuildContext context)
   {
-    var width = MediaQuery.of(context).size.width;
-
-    return CustomScrollView(
+    return const CustomScrollView(
       slivers: [
         SliverFillRemaining(
           hasScrollBody: false,
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30),
+            padding: EdgeInsets.symmetric(horizontal: 30),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                const CustomBookDetailsAppBar(),
+                CustomBookDetailsAppBar(),
 
-                const SizedBox(height: 20),
+                SizedBox(height: 20),
 
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: width * .2),
-                  child: const CustomBookImage(),
-                ),
+                BookDetailsSection(),
 
-                const SizedBox(height: 26),
+                Expanded(child: SizedBox(height: 50)),
 
-                Text('The Jungle Book', style: GoogleFonts.doHyeon(textStyle: const TextStyle(fontSize: 30))),
+                SimilarBooksSection(),
 
-                Text('Rudyard Kipling', style: GoogleFonts.doHyeon(textStyle: const TextStyle(fontSize: 20, color: greyColor))),
-
-                const SizedBox(height: 10),
-
-                const BookRatingOnly(),
-
-                const SizedBox(height: 29),
-
-                const BooksAction(),
-
-                const Expanded(child: SizedBox(height: 50)),
-
-                const Padding(
-                  padding: EdgeInsets.only(left: 6),
-                  child: Align(
-                      alignment: Alignment.centerLeft,
-                      child: Text('You can also like', style: TextStyle(fontSize: 14))
-                  ),
-                ),
-
-                const SizedBox(height: 8),
-
-                const BooksDetailsListView(),
-
-                const SizedBox(height: 15)
+                SizedBox(height: 15)
               ],
             ),
           ),
@@ -71,4 +38,3 @@ class BookDetailsViewBody extends StatelessWidget
     );
   }
 }
-
---
+++ lib/Features/Home/Presentation/Views/Widgets/books_details_section.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../../../../../constants.dart';
+import 'book_rating_only.dart';
+import 'books_action.dart';
+import 'custom_list_view_item.dart';
+
+class BookDetailsSection extends StatelessWidget
+{
+  const BookDetailsSection({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context)
+  {
+    var width = MediaQuery.of(context).size.width;
+
+    return Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: width * .2),
+            child: const CustomBookImage(),
+          ),
+
+          const SizedBox(height: 26),
+
+          Text('The Jungle Book', style: GoogleFonts.doHyeon(textStyle: const TextStyle(fontSize: 30))),
+
+          Text('Rudyard Kipling', style: GoogleFonts.doHyeon(textStyle: const TextStyle(fontSize: 20, color: greyColor))),
+
+          const SizedBox(height: 10),
+
+          const BookRatingOnly(),
+
+          const SizedBox(height: 29),
+
+          const BooksAction(),
+        ]
+    );
+  }
+}
