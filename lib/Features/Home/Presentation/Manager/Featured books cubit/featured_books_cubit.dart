748d7bcc661381bf83c633c028e2cd47ecc17d41
Mohammad Adel Metwalli <batootadel6@gmail.com>
2023-05-27 21:33:15 +0300

    Create featured books method

--- lib/Features/Home/Presentation/Manager/Featured books cubit/featured_books_cubit.dart
+++ lib/Features/Home/Presentation/Manager/Featured books cubit/featured_books_cubit.dart
@@ -1,11 +1,27 @@
+import 'package:bookly_app/Features/Home/Data/Repositories/home_repository.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import '../../../Data/Models/book_model.dart';
-
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState>
 {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepository) : super(FeaturedBooksInitial());
+
+  final HomeRepository homeRepository;
+
+  Future<void> fetchFeaturedBooks() async
+  {
+    emit(FeaturedBooksLoading());
+    var result = await homeRepository.fetchFeaturedBooks();
+
+    result.fold((failure)
+    {
+      emit(FeaturedBooksFailure(failure.errorMessage));
+    }
+    ,(books)
+    {
+      emit(FeaturedBooksSuccess(books));
+    });
+  }
 }
