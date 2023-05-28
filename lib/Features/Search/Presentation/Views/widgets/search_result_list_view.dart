f221ae38a37872b223a068a467d3dfc3ea5dc6e1
Mohammad Adel Metwalli <batootadel6@gmail.com>
2023-05-29 02:18:34 +0300

    Fix issue with loading data

--- lib/Features/Search/Presentation/Views/widgets/search_result_list_view.dart
+++ lib/Features/Search/Presentation/Views/widgets/search_result_list_view.dart
@@ -1,7 +1,8 @@
 import 'package:bookly_app/Features/Search/Presentation/Views/widgets/book_search_list_view_item.dart';
 import 'package:flutter/cupertino.dart';
 
-class SearchResultListView extends StatelessWidget {
+class SearchResultListView extends StatelessWidget
+{
   const SearchResultListView({Key? key}) : super(key: key);
 
   @override
